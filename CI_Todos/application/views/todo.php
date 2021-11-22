98497906906de27087d1200234f0e63d754abc7a
Mehmet Can Hanaylı <mehmet.hanayli@adu.edu.tr>
2021-11-22 17:29:04 +0300

    Todo List Frontend tamamlandı.

--- CI_Todos/application/views/todo.php
+++ CI_Todos/application/views/todo.php
@@ -1,20 +1,78 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todo List Uygulaması</title>
 
-    <?php $this->load->view("includes/style.php");?>
+    <?php $this->load->view("includes/style.php"); ?>
 
 </head>
+
 <body>
-    <h5>Merhaba TODO View çalıştı....</h5>
+
+    <h3 class="text-center">Todo List Uygulaması</h5>
+        <br>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <form action="" method="POST">
+                        <div class="row">
+                            <div class="col-md-11">
+                                <input type="text" class="form-control" placeholder="Planladığınız işlemi giriniz..">
+                            </div>
+                            <div class="col-md-1">
+                                <button class="btn btn-primary">Ekle </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <br>
+            <div class="row">
+                <table class="table table-bordered table-hover table-striped">
+
+                    <thead>
+                        <tr>
+                            <td class="text-center">Yapılacaklar</td>
+                            <td style="width: 100px;">Durum</td>
+                            <td style="width: 100px;">İşlem</td>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td>Ödevler Yapılacak</td>
+                            <td>Durum</td>
+                            <td><a href="" class="btn btn-danger"> Sil </a> </td>
+                        </tr>
+
+               
 
 
 
-    <?php $this->load->view("includes/script.php");?>
-  
+                    </tbody>
+
+
+                </table>
+
+
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+        <?php $this->load->view("includes/script.php"); ?>
+
 </body>
+
 </html>
